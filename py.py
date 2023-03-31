cd35247c24a607a5f615541690b59f47157b3bdb
andrean0122 <129488345+andrean0122@users.noreply.github.com>
2023-03-31 14:50:09 +0800

    simple python program

---
+++ py.py
@@ -0,0 +1,12 @@
+age=int(input())
+sex=input()
+if sex=='m':
+    if age>=16:
+        print('Mr.')
+    else:
+        print('Toto')
+elif sex=='f':
+    if age>=16:
+        print('Ms.')
+    else:
+        print('inday')
---
+++ sample.py
@@ -0,0 +1,4 @@
+cents=input("Enter number of scents: ")
+d=int(cents)//int(100)
+c=int(cents)% int(100)
+print(f"That is {d} dollars and {c} cents.")
